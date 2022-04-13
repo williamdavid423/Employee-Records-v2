8002ed9377a88358dc1ed6e23865357dd0bb3072
billy david <williamdavid423@hotmail.com>
2022-04-13 09:58:19 -0400

    needs more work done

--- EmployeeRecords/Form1.cs
+++ EmployeeRecords/Form1.cs
@@ -13,15 +13,25 @@ namespace EmployeeRecords
     public partial class mainForm : Form
     {
         List<Employee> employeeDB = new List<Employee>();
+        XmlReader reader = XmlReader.Create("XMLFile1.xml");
 
         public mainForm()
         {
             InitializeComponent();
+            loadDB();
         }
 
         private void addButton_Click(object sender, EventArgs e)
         { 
             ClearLabels();
+
+            string id  = idInput.Text;
+            string firstName = fnInput.Text;
+            string lastName = lnInput.Text;
+            string date = dateInput.Text;
+            string salary = salaryInput.Text;
+
+
         }
 
         private void removeButton_Click(object sender, EventArgs e)
@@ -31,7 +41,10 @@ namespace EmployeeRecords
 
         private void listButton_Click(object sender, EventArgs e)
         {
+            foreach(Employee emp in employeeDB)
+            {
 
+            }
         }
 
         private void ClearLabels()
@@ -41,6 +54,9 @@ namespace EmployeeRecords
             lnInput.Text = "";
             dateInput.Text = "";
             salaryInput.Text = "";
+
+            Employee employee = new Employee(id, firstName, lastName, date, salary);
+            employeeDB.Add(employee);
         }
 
         private void mainForm_FormClosed(object sender, FormClosedEventArgs e)
@@ -50,7 +66,32 @@ namespace EmployeeRecords
 
         public void loadDB()
         {
-            
+            XmlReader reader = XmlReader.Create("Resources/emplyeeData.xml");
+
+            while(reader.Read())
+            {
+                if (reader.NodeType == XmlNodeType.Text)
+                {
+                    string id = reader.ReadString();
+
+                    reader.ReadToNextSibling("FirstName");
+                    string firstName = reader.ReadString();
+
+                    reader.ReadToNextSibling("LastName");
+                    string lastName = reader.ReadString();
+
+                    reader.ReadToNextSibling("date");
+                    string date = reader.ReadString();
+
+                    reader.ReadToNextSibling("pay");
+                    string salary = reader.ReadString();
+
+                    Employee employee = new Employee(id, firstName, lastName, date, salary);
+                    employeeDB.Add(employee);
+                }
+            }
+            reader.Close();
+
         }
 
         public void saveDB()
